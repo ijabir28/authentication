b6cf7e67f701a4f00db5300d75003b0809e3cf60
jabir-st-dev <127714535+jabir-st-dev@users.noreply.github.com>
2023-03-23 15:09:48 +0600

    multer action updated
    multer action move to controller layer

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,24 @@
 const user_service = require('../services/user')
 
-exports.registration = function (req, res) {
+const multer = require("multer");
+const path = require("path");
+const DIR = 'public/files';
+
+let storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+        callback(null, DIR);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + (parseInt(Math.random() * 1000).toString()) + path.extname(file.originalname));
+    }
+});
+
+let upload = multer({storage: storage});
+
+
+exports.registration = function (req, res, next) {
     const user = {first_name, last_name, email, password, nid, age, marital_status} = req.body;
-    user.photo = 'public/files/' + req.file.filename;
+    user.photo = path.join(DIR, req.file.filename);
 
     user_service.registration(user).then(function (result) {
         res.status(200).send(result);
@@ -11,20 +27,6 @@ exports.registration = function (req, res) {
     });
 };
 
-// exports.upload = function (req, res, next) {
-//     const multer = require("multer");
-//
-//     const DIR = 'public/files';
-//
-//     let storage = multer.diskStorage({
-//         destination: function (req, file, callback) {
-//             callback(null, DIR);
-//         },
-//         filename: function (req, file, cb) {
-//             console.log(file.originalname);
-//             cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-//         }
-//     });
-//
-//     let upload = multer({storage: storage});
-// }
+exports.upload = function (req, res, next) {
+    upload.single('photo')(req, res, next);
+}
--- routes/user.js
+++ routes/user.js
@@ -3,9 +3,6 @@ const user_controller = require('../controllers/user')
 const express = require('express');
 const router = express.Router();
 
-const multer = require("multer");
-const upload = multer({ dest: "public/files" });
-
-router.post('/registration', upload.single('photo_file'), user_controller.registration)
+router.post('/registration', user_controller.upload, user_controller.registration)
 
 module.exports = router;
